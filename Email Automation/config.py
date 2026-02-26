8166601d27034c5224de688841fe4a6be232ebcc
Rodrigo Infante <roesinf2@gmail.com>
2026-02-26 14:59:13 -0500

    Remove sensitive email credentials from config.py
    Updated email configuration to remove sensitive information.

--- Email Automation/config.py
+++ Email Automation/config.py
@@ -11,9 +11,9 @@ DB_CONFIG = {
 DB_URL = f"postgresql://{DB_CONFIG['user']}:{quote_plus(DB_CONFIG['password'])}@{DB_CONFIG['host']}:{DB_CONFIG['port']}/{DB_CONFIG['database']}"
 
 EMAIL_CONFIG = {
-    "sender_email": "roesinf2@gmail.com",
-    "sender_password": "bvvo dgcp whkz ffzi",
-    "recipients": ["roesinf2@gmail.com", "valentinaburbano2002@gmail.com"],
+    "sender_email": "------------------",
+    "sender_password": "------------------",
+    "recipients": ["------------------", "------------------"],
     "smtp_server": "smtp.gmail.com",
     "smtp_port": 587,
 }
